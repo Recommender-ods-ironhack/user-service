3617ffb430a804a1044f4ff9d9a6088f37ceb058
Gab Caser <caserogabriela@gmail.com>
2025-05-22 15:41:57 +0200

    add save user

--- src/main/java/com/recommender/user_service/controller/UserController.java
+++ src/main/java/com/recommender/user_service/controller/UserController.java
@@ -6,10 +6,7 @@ import com.recommender.user_service.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -27,5 +24,13 @@ public class UserController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
         }
     }
+
+    @PostMapping()
+    public ResponseEntity<?> saveUser(@RequestBody User user){
+        var savedUser = userService.saveUser(user);
+        return ResponseEntity.ok(savedUser);
+    }
+
+
 
 }
