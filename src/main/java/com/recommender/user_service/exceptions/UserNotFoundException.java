baa746ee923f7c3ff0fcb7705f497330d101c531
Gab Caser <caserogabriela@gmail.com>
2025-05-22 15:15:50 +0200

    add usernotfound

--- src/main/java/com/recommender/user_service/exceptions/UserNotFoundException.java
+++ src/main/java/com/recommender/user_service/exceptions/UserNotFoundException.java
@@ -1,7 +1,7 @@
 package com.recommender.user_service.exceptions;
 
 public class UserNotFoundException extends RuntimeException {
-  public UserNotFoundException(String message) {
-    super(message);
-  }
+    public UserNotFoundException(Long id) {
+        super("User not found with ID: " + id);
+    }
 }
